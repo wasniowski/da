260a946fe9b919454d5b4581fdc1c55301e3f37e
kamil.wasniowski <kamil.wasniowski@jamf.com>
2021-09-01 22:32:35 +0200

    init

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
 (function ($) {
   var main = {
     init: function () {
-      // Enable Sliders
-      enableSliders.init();
-
       // Enable Navigation
       navigation.init();
 
-      // Enable Masonry Gallery
-      masonryGrid.init();
+      // Dynamic labels
+      dynamicLabels.init();
     },
   };
 
@@ -32,111 +29,63 @@
   };
 
   /*
-   * Enable Sliders
+   * Dynamic Labels
    */
 
-  var enableSliders = {
-    sliderCooperationSelector: ".js-slider-cooperation",
-    sliderCooperationBreakpoint: 768,
-    sliderCooperation: [],
-    sliderBlogSelector: ".js-slider-blog",
-    sliderBlogBreakpoint: 768,
-    sliderBlog: [],
+  var dynamicLabels = {
+    formSelector: ".js-dl-form",
+    itemSelector: ".js-dl-item",
 
     init: function () {
       var _self = this;
 
-      // toggle slider cooperation
-      this.toggleSliderCooperation();
+      var fieldsObj =
+        this.formSelector +
+        ' input[type="text"], ' +
+        this.formSelector +
+        ' input[type="email"], ' +
+        this.formSelector +
+        ' input[type="number"], ' +
+        this.formSelector +
+        ' input[type="tel"], ' +
+        this.formSelector +
+        ' input[type="password"], ' +
+        this.formSelector +
+        " textarea";
 
-      // toggle slider blog
-      this.toggleSliderBlog();
+      $(fieldsObj).each(function () {
+        _self.setFilled(this);
+      });
 
-      // toggle sliders on window resize
-      var resizeTimeout = null;
-      $(window).resize(function () {
-        clearTimeout(resizeTimeout);
-        resizeTimeout = setTimeout(function () {
-          _self.toggleSliderCooperation();
-          _self.toggleSliderBlog();
-        }, 100);
+      $(document).on("focus", fieldsObj, function () {
+        $(this).closest(_self.itemSelector).addClass("filled");
+      });
+
+      $(document).on("blur", fieldsObj, function () {
+        var obj = this;
+        _self.setFilled(obj);
+      });
+
+      $(document).on("change", fieldsObj, function () {
+        _self.setFilled(this);
+      });
+
+      $(document).on("click", this.itemSelector, function () {
+        $(this)
+          .find(
+            'input[type="text"], input[type="email"], input[type="number"], input[type="tel"], input[type="password"], textarea'
+          )
+          .focus();
       });
     },
 
-    toggleSliderCooperation: function () {
-      var _self = this;
-
-      if ($(window).width() < this.sliderCooperationBreakpoint) {
-        if (
-          this.sliderCooperation.length == 0 &&
-          $(this.sliderCooperationSelector).length
-        ) {
-          var sliderCooperationOptions = {
-            pager: false,
-            maxSlides: 4,
-            slideWidth: 260,
-            moveSlides: 1,
-            controls: false,
-            adaptiveHeight: true,
-            slideMargin: 25,
-          };
-          $(this.sliderCooperationSelector).each(function () {
-            _self.sliderCooperation.push(
-              $(this).bxSlider(sliderCooperationOptions)
-            );
-          });
-        }
+    setFilled: function (obj) {
+      if ($(obj).val() != "") {
+        $(obj).closest(this.itemSelector).addClass("filled");
       } else {
-        if (this.sliderCooperation.length) {
-          for (var i in this.sliderCooperation) {
-            this.sliderCooperation[i].destroySlider();
-          }
-
-          this.sliderCooperation = [];
-        }
+        $(obj).closest(this.itemSelector).removeClass("filled");
       }
     },
-
-    toggleSliderBlog: function () {
-      var _self = this;
-
-      if ($(window).width() < this.sliderBlogBreakpoint) {
-        if (this.sliderBlog.length == 0 && $(this.sliderBlogSelector).length) {
-          var sliderBlogOptions = {
-            pager: false,
-            maxSlides: 4,
-            slideWidth: 260,
-            moveSlides: 1,
-            controls: false,
-            adaptiveHeight: true,
-            slideMargin: 25,
-          };
-          $(this.sliderBlogSelector).each(function () {
-            _self.sliderBlog.push($(this).bxSlider(sliderBlogOptions));
-          });
-        }
-      } else {
-        if (this.sliderBlog.length) {
-          for (var i in this.sliderBlog) {
-            this.sliderBlog[i].destroySlider();
-          }
-
-          this.sliderBlog = [];
-        }
-      }
-    },
-  };
-
-  /*
-   * Masonry Grid
-   */
-
-  var masonryGrid = {
-    init: function () {
-      if($('.mansory-grid').length){
-        $('.masonry-grid').masonryGrid();
-      }
-    }
   };
 
   $(function () {
